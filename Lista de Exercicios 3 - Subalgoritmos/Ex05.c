3cd1e8affdec2c4ec7159bf12d343d8b6547c018
Guilherme Barros <guilhermebarrosr@gmail.com>
2019-10-31 09:18:52 -0300

    Alteracao no ex02 da lista 3

---
+++ Lista de Exercicios 3 - Subalgoritmos/Ex05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/*Escrevaum programa contendo funções ou procedimentos para realizar as seguintes tarefas:
+a.Calcular o resto da divisão inteira de a por b
+b.Exibir um inteiro entre 1 e 999 como uma sequênciade dígitos,separando cada par de dígitos por dois espaços.
+Por exemplo, o inteiro 456 deve ser apresentado como 4 5 6.
+c.Imprimir os múltiplos de 7 menores que 200.*/
+
+
+void main(){
+    
+
+}
+
+int restoDivisao(int a, int b){
+    return a % b;
+}
--- Lista3/Questão2.c
+++ Lista3/Questão2.c
@@ -6,25 +6,33 @@ n�meros �mpares do intervalo fechado de 1 a X.*/
 
 
 void imprimeImpares(int numero);
+void isImpar(int num);
 
 void main(){
 
     int valor;
     printf("Digite a quantidade de valores para rodar: ");
     scanf("%d",&valor);
-    imprimeImpares(valor);
+    imprimeImpar(valor);
 
 }
-void imprimeImpares(int numero){
-    
-    for (int i = 1; i <= numero; i++)
-    {
-        if (i % 2 != 0)
-        {
-            printf("%d\n", i);
-        }
+void imprimeImpar(int num)
+{
+    if(num == 1){
+    isImpar(num);
+        
+    }else{
+        
+        imprimeImpar(num - 1);
+        isImpar(num);
+
     }
-    
 
     
+}
+void isImpar(int num){
+    if (num % 2 != 0)
+    {
+        printf("%d\n", num);
+    }
 }
